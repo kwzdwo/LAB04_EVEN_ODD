ac894cb964ae46b3cc1355adc76545680d49a34f
KojoMensah <kwadwom200@gmail.com>
2024-09-23 23:51:43 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+       int numToExamine = 2;
+
+       if (numToExamine % 2 == 0);
+        {
+            System.out.println(" Is even");
+        } else {
+        System.out.println( "Is Odd");
+    }
+
+
+    }
+}
